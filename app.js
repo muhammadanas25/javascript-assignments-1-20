12c4585a5eb7127369055f73becce367760b71fd
muhammadanas25 <58306914+muhammadanas25@users.noreply.github.com>
2020-06-14 17:56:19 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,350 @@
+//==== Chapter 1 , Task 1
+//alert("Hello World")
+
+//==== Chapter 1 , Task 2
+//alert("Error ! Please Enter a valid Password")
+
+//==== Chapter 1 , Task 3
+//alert("Welcome to JS Land...\n Happy Coding")
+
+//==== Chapter 1 , Task 4
+//alert("Welcome to JS Land...")
+//alert("Happy Coding")
+
+//==== Chapter 1 , Task 5&6
+{/* <input type="button" id="button" value="Button">
+
+  button.onclick = function() {
+    alert(' Hello...I can run JS through my Web Browser Console');
+  };
+ */}
+
+//==== Chapter 1 , Task 7
+{/*
+    
+      
+          document.getElementById("demo").innerHTML = "Hello JavaScript!"; 
+    
+*/}
+
+
+
+
+
+
+
+//==== Chapter 2 , Task 1
+//var username
+
+
+//==== Chapter 2 , Task 2
+// var myName 
+// myName="Muhmmad ANAS "
+
+
+//==== Chapter 2 , Task 3
+// var message
+// message="Hello World"
+// alert(message)
+
+//==== Chapter 2 , Task 4
+// var name 
+// var age
+// var course
+// name="Jhone Doe"
+// age="15 years old"
+// course="Certified Mobile Application Development"
+// alert(name)
+// alert(age)
+// alert(course)
+
+//==== Chapter 2 , Task 5
+// x="PIZZA\nPIZZ\nPIZ\nPI\nP"
+// alert(x)
+
+//==== Chapter 2 , Task 6
+// var email="anassiddiqui"
+// alert("My email address is "+" "+ email)
+
+//==== Chapter 2 , Task 7
+// var book="A smarter way to learn JavaScript"
+// alert("I am trying to learn from the book "+book )
+
+//==== Chapter 2 , Task 8
+//Yayy! I can wrie HTML content through JavaScript
+
+
+//==== Chapter 2 , Task 9
+// var symbol="▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬"
+// alert(symbol)
+
+
+//==== Chapter 3 , Task 1
+// var age = 18 
+// alert("I am "+ age +" years old.")
+
+//==== Chapter 3 , Task 2
+
+// var visit = 1
+// var numberofvisits = visit + 1
+// alert("You Have visited this site "+numberofvisits+" times")
+// visit = visit +  1
+
+//==== Chapter 3 , Task 3
+// var birthyear = 2001
+// alert("My birth year is " + birthyear+ "\nData type of my declared variacle is number ")
+
+//==== Chapter 3 , Task 4
+
+// var  Visitorsname ,Producttitle,Quantity
+// alert(Visitorsname +" ordered " + Quantity + " T-shirt(s) on XYZ Clothing store")
+
+
+
+
+
+
+
+
+
+
+
+
+//==== Chapter 5 , Task 12
+//No Solution
+
+
+
+//=== chapter 6 task 1
+// var a = 10;
+// document.write("Result \n"  ); 
+// document.write("the value of a is " , a  );
+
+// document.write("---------------"   );
+
+// document.write("the value of ++a is " , ++a  );
+// document.write("now the value of a++ is " , a++  );
+
+// document.write("the value of a++ is " , a++  );
+// document.write("nowthe value of a++ is " , a++  );
+
+// document.write("the value of --a is " , --a  );
+// document.write("now the value of --a is " , --a  );
+
+// document.write("the value of a-- is " , a--  );
+// document.write("now the value of a-- is " , a--  );
+
+//=== chapter 6 task 2
+// var a=2 ;
+// var b=1;
+// var result = --a - --b + ++b + b--;
+
+// document.write (result);
+
+// document.write(--a);
+// document.write(--a - --b);
+// document.write(--a - --b + ++b);
+// document.write(--a - --b + ++b + b--);
+
+//=== chapter 6 task 3
+
+//var name = prompt(" please enter your name");
+//document.write("welcome "+ name );
+
+//=== chapter 6 task 4,5
+
+//var num = prompt(" please enter a number);
+//var count ;
+//for(count=0;count <10 ; count++ ){
+//   document.write(num + " * " + num + " = " + num*num );
+ //   document.write("<br />")
+//}
+
+//chapter 6-9 task 6,7
+ 
+// var subject1=prompt("enter the name of subject 1 ");
+// var subject2=prompt("enter the name of subject 2 ");
+// var subject3=prompt("enter the name of subject 3 ");
+// var totalmark=100;
+
+// var subject1marks=prompt("enter the marks of subject 1 ");
+// var subject2marks=prompt("enter the marks of subject 2 ");
+// var subject3marks=prompt("enter the marks of subject 3 ");
+
+// document.write("Subject  total  obtained percentage ");
+// document.write("<br>");
+// document.write(subject1 + "  " + totalmark + "  " + subject1marks  + "  "+ (subject1marks / totalmark )* 100);
+// document.write("<br>");
+// document.write(subject2 + "  " + totalmark + "  " + subject2marks  + "  "+ (subject2marks / totalmark )* 100);
+// document.write("<br>");
+// document.write(subject3 + "  " + totalmark + "  " + subject3marks  + "  "+ (subject3marks / totalmark )* 100);
+
+//chapter 9-10 task 1
+// var city = prompt(" please enter city name");
+
+// if (city.toLowerCase === "karachi");
+// {
+//     document.write( "Welcome to city of lights");
+// }
+//chapter 9-10 task 2
+
+// var gender = prompt(" please enter your gender ");
+// if (gender.toLowerCase === "male")
+// {
+//  document.write( "good morning sir ");
+// }
+// else  if (gender.toLowerCase === "female" )
+// {
+//     document.write( "good morning maam ");
+// }
+
+//chapter 9-10 task 3
+
+// var color = prompt(" please enter color");
+
+//  if (color == "red")
+//  {
+//      document.write( "must stop");
+//  }
+// else if (color == "yellow")
+// {
+//     document.write( "Ready to move    ");
+// }
+
+// else if (color == "green")
+// {
+//     document.write( "Move now");
+// }
+
+// var color = prompt(" please enter color");
+
+//chapter 9-10 task 4
+// var fuel = prompt(" please enter remaining feul in liters");
+//  if (fuel < "0.25")
+//  {
+//      document.write( "Please refill the fuel in your car");
+// }else document.write("Continue")
+
+
+//chapter 9-10 task 5
+// a) Displayed 
+// b) Not Displayed
+// c) Displayed
+// d) Not Displayed
+// e) Displayed
+// f) Displayed
+
+// var TMark= prompt("Plese Enter Your Total Marks");
+// var Mark= prompt("Plese Enter Your Marks Obtained");
+
+// Marks= Mark*100/TMark
+
+
+//   if (Marks > 79)
+//   {
+//       document.write( "MarkSheet\n"+"\n  Total Marks :"+TMark+ " \n  Marks Obtained: "+Mark+ " Percentage: " + Marks + "  Grade:A-one");
+//  }else if  (Marks > 69)
+//  {
+//      document.write("MarkSheet\n"+"\n  Total Marks :"+TMark+ " \n  Marks Obtained: "+Mark+ " Percentage: " + Marks + "  Grade:A");
+// }else   if (Marks > 59)
+// {
+//     document.write("MarkSheet\n"+"\n  Total Marks :"+TMark+ " \n  Marks Obtained: "+Mark+ " Percentage: " + Marks + "  Grade:B");
+// }else   if (Marks < 60 )
+// {
+//     document.write( "MarkSheet\n"+"\n  Total Marks :"+TMark+ " \n  Marks Obtained: "+Mark+ " Percentage: " + Marks +"  Grade:Fail");
+// }
+
+//chapter 11-13  task 2
+
+// var n= prompt("Plese Enter number 1");
+// var m= prompt("Plese Enter number 2");
+
+// if(n>m){
+//     document.write(n + "is larger");
+// }
+// else if(n<m){
+//     document.write(m + "is larger");
+// }
+// else{
+//     document.write("equal");
+// }
+
+
+
+//chapter 11-13  task 3
+
+// var n= prompt("Plese Enter number 1");
+
+
+//  if(n>0){
+//      document.write(n + "is psitive");
+//  }
+//  else if(n<0){
+//      document.write(m + "is negative");
+//  }
+//  else{
+//     document.write("zero");
+//  }
+
+//chapter 11-13  task 4
+//  var n= prompt("Plese Enter letter");
+
+
+//  if(n== "u" ||n== "a" ||n== "e" ||n== "i" ||n== "o"  ){
+//      document.write("true");
+//  }
+// else{
+//     document.write("false");
+// }
+
+//chapter 11-13  task 6
+// var hour = 13;
+// if (hour < 18) {
+//     greeting = "Good day";
+// }
+// else{
+//     greeting = "Good evening";
+// }
+
+//chapter 13-16 task 1-9
+// var  name= [];
+// var cars = [ "Volvo", "BMW"];
+// var n = [1,2,4,3];
+// var x = [true,false];
+// var fas = [1,"Saab", 2,"Volvo", "BMW"];
+// var qualification=["SSC", "HSC", "BCS",
+//     "BS", "BCOM", "MS"," M. Phil," "PhD"];
+//     for(var c=0;c<8;c++){
+//         document.write(c+ ")  "qualification[c] );
+//         document.write("<br>");
+//     }
+
+// //chapter 17-20 task 1-3
+// var activities = [];
+// var num =   [0, 1,2,3],
+// [1, 0,1,2],
+// [2,1,0,1],
+// ];
+
+
+//chapter 17-20 task 3
+// var c =0;
+// for(c=1;c<=10 ;c++){
+//     document.write(c);
+//     document.write("<br>");
+// }
+
+//chapter 17-20 task 4
+
+//var num = prompt(" please enter a number);
+//var x = prompt(" please enterlenth of table);
+//var count ;
+//for(count=1;count <=x ; count++ ){
+//   document.write(num + " * " + count + " = " + num*count  );
+ //   document.write("<br />")
+//}
+
+
+//chapter 17-20 task 5
+// fruits = [“apple”, “banana”, “mango”, “orange”,
+//     “strawberry”]
